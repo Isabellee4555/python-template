9305ee17afd64f3441d02067660b5165f1a6b3ff
Isabel Li <isabellee4555@gmail.com>
2025-07-22 21:18:55 +1000

    Added mypy to precommit

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,26 +4,26 @@ from utils import add, divide, multiply, subtract
 
 
 @pytest.mark.isabel
-def test_add():
+def test_add() -> None:
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
 
 
 @pytest.mark.isabel
-def test_subtract():
+def test_subtract() -> None:
     assert subtract(5, 3) == 2
     assert subtract(0, 0) == 0
     assert subtract(-1, -1) == 0
 
 
-def test_multiply():
+def test_multiply() -> None:
     assert multiply(2, 3) == 6
     assert multiply(-1, 1) == -1
     assert multiply(0, 0) == 0
 
 
-def test_divide():
+def test_divide() -> None:
     assert divide(6, 3) == 2
     assert divide(-1, 1) == -1
     with pytest.raises(ValueError):
